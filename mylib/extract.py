e6db08944418e40f9998142c7d441bbe5cbe564a
周诗玥 <CynthiaSY@zhouzhihuadeMacBook-Pro.local>
2024-11-22 13:32:22 -0500

    test

--- mylib/extract.py
+++ mylib/extract.py
@@ -1,41 +1,41 @@
-"""
-Extract a dataset from a URL like Kaggle or data.gov. 
-JSON or CSV formats tend to work well
+# """
+# Extract a dataset from a URL like Kaggle or data.gov. 
+# JSON or CSV formats tend to work well
 
-food dataset
-"""
-import os
-import requests
+# food dataset
+# """
+# import os
+# import requests
 
 
 
-def extract(
-    url="https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/murder_2016/murder_2015_final.csv",
-    file_path="data/murder_2015_final.csv",
-):
-     # Create the directory if it doesn't exist
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+# def extract(
+#     url="https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/murder_2016/murder_2015_final.csv",
+#     file_path="data/murder_2015_final.csv",
+# ):
+#      # Create the directory if it doesn't exist
+#     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     
-    # Fetch the content from the URL
-    response = requests.get(url)
+#     # Fetch the content from the URL
+#     response = requests.get(url)
     
-    # Check for valid response status
-    if response.status_code == 200:
-        # Save the content to the specified file path
-        with open(file_path, "wb") as f:
-            f.write(response.content)
-        print(f"File successfully downloaded to {file_path}")
-    else:
-        print(f"Failed to retrieve the file. HTTP Status Code: {response.status_code}")
+#     # Check for valid response status
+#     if response.status_code == 200:
+#         # Save the content to the specified file path
+#         with open(file_path, "wb") as f:
+#             f.write(response.content)
+#         print(f"File successfully downloaded to {file_path}")
+#     else:
+#         print(f"Failed to retrieve the file. HTTP Status Code: {response.status_code}")
     
-    return file_path   
+#     return file_path   
     
-    # """ "Extract a url to a file path"""
-    # with requests.get(url) as r:
-    #     with open(file_path, "wb") as f:
-    #         f.write(r.content)
-    # return file_path
+#     # """ "Extract a url to a file path"""
+#     # with requests.get(url) as r:
+#     #     with open(file_path, "wb") as f:
+#     #         f.write(r.content)
+#     # return file_path
 
 
-if __name__ == "__main__":
-    extract()
+# if __name__ == "__main__":
+#     extract()
--- mylib/query.py
+++ mylib/query.py
@@ -1,74 +1,74 @@
-"""Query the database"""
+# """Query the database"""
 
-import os
-from databricks import sql
-from dotenv import load_dotenv
+# import os
+# from databricks import sql
+# from dotenv import load_dotenv
 
-murder_execute_query="""
-WITH murder_change_rate AS(
-          SELECT  
-              city,
-              state_s,
-              try_divide(change,murders2014) AS Murder_Change_Rate
-          FROM default.Murder2015)
-
-SELECT 
-    a.state_s,
-    AVG(b.Murder_Change_Rate) AS average_murdersChange_perState
-FROM default.Murder2015 a
-LEFT JOIN murder_change_rate b ON a.city = b.city AND a.state_s = b.state_s
-GROUP BY a.state_s
-ORDER BY average_murdersChange_perState DESC;
-"""
-
-# murder_second_query = """
-#         WITH murder_change_rate AS(
+# murder_execute_query="""
+# WITH murder_change_rate AS(
 #           SELECT  
 #               city,
 #               state_s,
 #               try_divide(change,murders2014) AS Murder_Change_Rate
 #           FROM default.Murder2015)
 
-#             SELECT * FROM murder_change_rate;
-
-#         """
-
-# averageMurder_perState_query = """
-#     SELECT 
-#         state_s,
-#         AVG(murders2015) AS average_murders2015_perState
-#     FROM default.Murder2015
-#     GROUP BY state_s
+# SELECT 
+#     a.state_s,
+#     AVG(b.Murder_Change_Rate) AS average_murdersChange_perState
+# FROM default.Murder2015 a
+# LEFT JOIN murder_change_rate b ON a.city = b.city AND a.state_s = b.state_s
+# GROUP BY a.state_s
+# ORDER BY average_murdersChange_perState DESC;
 # """
 
-# SortingMurder_perState_query = """
-#     SELECT 
-#         state_s,
-#         AVG(murders2015) AS average_murders2015_perState
-#     FROM default.Murder2015
-#     GROUP BY state_s
-#     ORDER BY average_murders2015_perState DESC
-# """
+# # murder_second_query = """
+# #         WITH murder_change_rate AS(
+# #           SELECT  
+# #               city,
+# #               state_s,
+# #               try_divide(change,murders2014) AS Murder_Change_Rate
+# #           FROM default.Murder2015)
 
-def query():
-    load_dotenv()
-    server_h = os.getenv("SQL_SERVER_HOST")
-    access_token = os.getenv("DATABRICKS_API_KEY")
-    http_path = os.getenv("SQL_HTTP_PATH")
-    with sql.connect(
-        server_hostname=server_h,
-        http_path=http_path,
-        access_token=access_token,
-    ) as connection:
-        c = connection.cursor()
-        #create second query + join + agggregation +sorting
-        c.execute(murder_execute_query)
+# #             SELECT * FROM murder_change_rate;
+
+# #         """
+
+# # averageMurder_perState_query = """
+# #     SELECT 
+# #         state_s,
+# #         AVG(murders2015) AS average_murders2015_perState
+# #     FROM default.Murder2015
+# #     GROUP BY state_s
+# # """
+
+# # SortingMurder_perState_query = """
+# #     SELECT 
+# #         state_s,
+# #         AVG(murders2015) AS average_murders2015_perState
+# #     FROM default.Murder2015
+# #     GROUP BY state_s
+# #     ORDER BY average_murders2015_perState DESC
+# # """
+
+# def query():
+#     load_dotenv()
+#     server_h = os.getenv("SQL_SERVER_HOST")
+#     access_token = os.getenv("DATABRICKS_API_KEY")
+#     http_path = os.getenv("SQL_HTTP_PATH")
+#     with sql.connect(
+#         server_hostname=server_h,
+#         http_path=http_path,
+#         access_token=access_token,
+#     ) as connection:
+#         c = connection.cursor()
+#         #create second query + join + agggregation +sorting
+#         c.execute(murder_execute_query)
     
-        result = c.fetchall()
-        print(result)
-        c.close()
-        return("success")
+#         result = c.fetchall()
+#         print(result)
+#         c.close()
+#         return("success")
 
 
-if __name__ == "__main__":
-    query()
+# if __name__ == "__main__":
+#     query()
